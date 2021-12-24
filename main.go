6cef3fd19c2b7b2c3813eaf2d7f83289c03df4c5
jt.boron <JT.BORON@GMAIL.COM>
2021-12-24 06:18:32 -0500

    restructred,added base routes and started with device prefix

--- main.go
+++ main.go
@@ -6,19 +6,35 @@ import (
 	"log"
 	"github.com/gorilla/mux"
 )
-
+//====================================================================
+func deviceAdd(w http.ResponseWriter, r *http.Request){
+    vars := mux.Vars(r)
+    key := vars["id"]
+    fmt.Fprintf(w, "Key: " + key)
+}
 //====================================================================
 func deviceStatus(w http.ResponseWriter, r *http.Request){
     vars := mux.Vars(r)
     key := vars["id"]
-
     fmt.Fprintf(w, "Key: " + key)
+}
+
+//====================================================================
+func apiStatus(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Rest API v1.0 - Device api mockup")
 }
 
 //====================================================================
 func handleRequests() {
     myRouter := mux.NewRouter().StrictSlash(true)
-    myRouter.HandleFunc("/status/{id}", deviceStatus)
+    myRouter.HandleFunc("/", apiStatus).Methods("GET")
+
+	
+	devicesrouter := myRouter.PathPrefix("/device").Subrouter()
+	devicesrouter.HandleFunc("", deviceAdd).Methods("POST")
+	devicesrouter.HandleFunc("/status/{id}", deviceStatus).Methods("GET")
+	
+	
     log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
